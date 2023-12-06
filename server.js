fefb8027f8e1f25f2b2f582390d1127197cc4bf3
mubashirrehman9 <brotherqureshi123456789@gmail.com>
2023-12-06 22:52:19 +0500

    removed url encoded

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const session = require('express-session');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-app.use(express.urlencoded({extended: false}));
 // Importing Routes
 const userRoute = require('./routes/User')
 const socialRoute = require('./routes/Social')
