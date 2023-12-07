91d758868d94419c62c707336a774cc256f591e7
mubashirrehman9 <brotherqureshi123456789@gmail.com>
2023-12-07 19:39:28 +0500

    added socket implementation

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require('express');
 const session = require('express-session');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path =require('node:path');
 // Importing Routes
 const userRoute = require('./routes/User')
 const socialRoute = require('./routes/Social')
+const cors = require('cors'); // Import the 'cors' middleware
 
 
 // Create the Express app
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
@@ -21,6 +27,15 @@ app.use(session({
 app.use("/user", userRoute)
 app.use("/social", socialRoute)
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/test.html'));
+});
+
+io.on('connection', async (socket) => {
+  console.log("User Connected "+ socket.id);
+});
+
+
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/friend-request-system?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -31,6 +46,6 @@ mongoose.connect('mongodb://localhost:27017/friend-request-system?retryWrites=tr
   });
 
 // Start the server
-app.listen(3000, () => {
+http.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
