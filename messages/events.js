d407081512c7b0d887cdac2a21623fdbd6797a87
mubashirrehman9 <brotherqureshi123456789@gmail.com>
2023-12-11 19:07:59 +0500

    Updated setUserStatusOnline event now user can only login at one place

--- messages/events.js
+++ messages/events.js
@@ -6,9 +6,21 @@ function IntializeSocketEvents(io) {
     io.on('connection', async (socket) => {
         console.log("User Connected " + socket.id);
         socket.on('setUserStatusOnline', async (username) => {
-            await utils.setUserStatusOnline(username)
-            sockets.push(socket.id);
-            playersLookup[socket.id] = username;
+            var canLogin = true
+            for (const [key, value] of Object.entries(playersLookup)) {
+                if(value == username){
+                    console.log(`${value} already logged in`);
+                    socket.emit("logginFailed",{reason:"Player already logged in!"});
+                    canLogin = false;
+                    break;
+                }
+            }
+            if(canLogin){
+                console.log("Logging In...")
+                await utils.setUserStatusOnline(username)
+                sockets.push(socket.id);
+                playersLookup[socket.id] = username;
+            }
         });
         socket.on('inviteUser', async (self,username,conInfo) => {
 
